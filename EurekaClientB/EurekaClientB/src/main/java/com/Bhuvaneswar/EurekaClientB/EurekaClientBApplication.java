39061b37d72c8de625d0af395929e0fe6727db59
origin <bhuvaneswarjakka99@gmail.com>
2024-07-01 19:52:03 +0530

    ClientB

---
+++ EurekaClientB/EurekaClientB/src/main/java/com/Bhuvaneswar/EurekaClientB/EurekaClientBApplication.java
@@ -0,0 +1,13 @@
+package com.Bhuvaneswar.EurekaClientB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientBApplication.class, args);
+	}
+
+}
