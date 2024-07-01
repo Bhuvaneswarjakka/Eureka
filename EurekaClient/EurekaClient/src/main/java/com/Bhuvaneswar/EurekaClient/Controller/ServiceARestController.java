8ac117074f139be80e040c85cf27b44fee9ccbc1
origin <bhuvaneswarjakka99@gmail.com>
2024-07-01 19:50:59 +0530

    ClientA

---
+++ EurekaClient/EurekaClient/src/main/java/com/Bhuvaneswar/EurekaClient/Controller/ServiceARestController.java
@@ -0,0 +1,13 @@
+package com.Bhuvaneswar.EurekaClient.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceARestController
+{
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello world from Service A!";
+    }
+}
